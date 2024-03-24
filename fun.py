f61dfe60760f9059d4fc10c404aa26ffd648b6f5
Muhammad Ahsan Mushtaq <ahsanmushtaq1373@gmail.com>
2024-03-24 17:21:27 +0500

    fun using python

---
+++ fun.py
@@ -0,0 +1,11 @@
+def draw_face():
+    print("       .--.")
+    print("      |o_o |")
+    print("      |:_/ |")
+    print("     //   \\ \\")
+    print("    (|     | )")
+    print("   /'\\_   _/`\\")
+    print("   \\___)=(___/")
+
+# Call the function to draw the face
+draw_face()
