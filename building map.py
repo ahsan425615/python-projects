128e67f576dcfb8dc317f6a3829e4d4d71ecc168
Muhammad Ahsan Mushtaq <ahsanmushtaq1373@gmail.com>
2024-03-24 17:28:36 +0500

    testing project in python

---
+++ building map.py
@@ -0,0 +1,25 @@
+# import folium
+
+# # Create a map centered around a specific location
+# mymap = folium.Map(location=[37.7749, -122.4194], zoom_start=12)
+
+# # Save the map as an HTML file
+# mymap.save("mymap.html")
+
+
+
+# import folium
+
+# # Create a map centered around a specific location
+# mymap = folium.Map(location=[37.7749, -122.4194], zoom_start=12)
+
+# # Add a marker
+# folium.Marker(location=[37.7749, -122.4194], popup="San Francisco").add_to(mymap)
+
+# # Save the map as an HTML file
+# mymap.save("mymap_with_marker.html")
+
+# import folium
+
+# m = folium.Map(location=(45.5236, -122.6750))
+
